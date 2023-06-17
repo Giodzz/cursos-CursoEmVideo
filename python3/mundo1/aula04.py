ecc096099429417585baafc383c65c9df9a6335e
Giodzz <giordanabbucci@gmail.com>
2023-06-17 00:39:16 -0300

    new file: aula04 - primeiros comandos em python3

---
+++ python3/mundo1/aula04.py
@@ -0,0 +1,9 @@
+# função de mostrar na tela
+print("Olá mundo!")
+
+# variáveis: guardar informação
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+peso = input("Quanto você pesa? ")
+
+print(nome, idade, peso)
