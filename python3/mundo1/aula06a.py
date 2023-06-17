e6172a2fa675c49435d1854b7cba0e8d0d3c33dd
Giodzz <giordanabbucci@gmail.com>
2023-06-17 01:02:29 -0300

    aula6: tipos primitivos e saída de dados

---
+++ python3/mundo1/aula06a.py
@@ -0,0 +1,10 @@
+# por padrão, o input recebe uma string
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite mais um número: "))
+s = n1 + n2
+
+# string formatada
+print(f"A soma vale {s}")
+
+# tipos primitivos de dados
+# int, float, bool e str
---
+++ python3/mundo1/aula06b.py
@@ -0,0 +1,4 @@
+n = str(input("Digite um valor: "))
+print(n.isnumeric())
+print(n.isalnum())
+print(n.isupper())
