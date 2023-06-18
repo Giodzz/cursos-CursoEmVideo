48570cd7f8b01af269136ee34b5fcd42f7e8b225
Giodzz <giordanabbucci@gmail.com>
2023-06-18 16:23:30 -0300

    aula07: operadores aritméticos

---
+++ python3/mundo1/aula07a.py
@@ -0,0 +1,14 @@
+# adição: +    subtração: -   multiplicação: *    divisão: /
+# potenciação: **   divisão inteira: //    módulo(resto): %
+
+# ordem de precedência: (), **, * / // %, + -
+
+# python representa números gigantescos
+
+# formatações do print
+nome = input("Qual é o seu nome? ")
+print(f"Prazer em te conhecer, {nome:20}!")
+print(f"Prazer em te conhecer, {nome:>20}!")
+print(f"Prazer em te conhecer, {nome:^20}!")
+print(f"Prazer em te conhecer, {nome:-^20}!")
+
---
+++ python3/mundo1/aula07b.py
@@ -0,0 +1,9 @@
+n1 = int(input("n1: "))
+n2 = int(input("n2: "))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print(f"A soma é {s}, o produto é {m} e a divisão é {d}", end=" --- ")
+print(f"divisão inteira {di} e potência {e}")
