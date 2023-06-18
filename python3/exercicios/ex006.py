237941746fc0e7a4fd151b9196e6902e2c6f9bfb
Giodzz <giordanabbucci@gmail.com>
2023-06-18 16:33:09 -0300

    ex006: dobro, triplo, raiz quadrada

---
+++ python3/exercicios/ex006.py
@@ -0,0 +1,7 @@
+from math import sqrt
+
+n = int(input("Digite um número inteiro: "))
+
+print(f"O dobro de {n} vale {n*2}.")
+print(f"O triplo de {n} vale {n*3}.")
+print(f"A raíz quadrada de {n} é igual a {sqrt(n):.2f}.")
