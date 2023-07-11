24332b394df250c09cea2652ee2aec7dd1e8a728
Giodzz <giordanabbucci@gmail.com>
2023-07-11 19:44:42 -0300

    aula10: condições (parte 1)

---
+++ python3/mundo1/aula10b.py
@@ -0,0 +1,6 @@
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+
+# utilizando condicional simplificada
+print("Sua média foi boa! PARABÉNS!" if m >= 6 else "Estude mais")
