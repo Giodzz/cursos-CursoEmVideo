ceae7f371f0a9a138518bc52dcca11e896a56987
Giodzz <giordanabbucci@gmail.com>
2023-07-15 17:29:41 -0300

    aulaExtra: cores no terminal

---
+++ python3/mundo1/aulaExtra.py
@@ -0,0 +1,26 @@
+# como mexer com cores no terminal
+# ANSI - escape sequence
+# Style: 0 none; 1 bold; 4 underline; 7 neagtive
+# text: 30 - 37
+# background: 40 - 47
+# formato: \033[Style;Text;Backm
+
+
+print("\033[0;30mPreto!\033[m")
+print("\033[1;31mVermelho!\033[m")
+print("\033[4;32mVerde!\033[m")
+print("\033[7;33mAmarelo!\033[m")
+print("\033[34mAzul!\033[m")
+print("\033[35mLilás!\033[m")
+print("\033[36mCiano!\033[m")
+print("\033[37mCinza!\033[m")
+
+print("\033[0;40mPreto!\033[m")
+print("\033[1;41mVermelho!\033[m")
+print("\033[4;42mVerde!\033[m")
+print("\033[7;43mAmarelo!\033[m")
+print("\033[44mAzul!\033[m")
+print("\033[45mLilás!\033[m")
+print("\033[46mCiano!\033[m")
+print("\033[47mCinza!\033[m")
+
