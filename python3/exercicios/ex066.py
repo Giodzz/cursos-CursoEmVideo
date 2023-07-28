d0470d759be631ed93b5b19b9a9eec93353ebcda
Giodzz <giordanabbucci@gmail.com>
2023-07-28 15:27:04 -0300

    ex066: vários números com flag

---
+++ python3/exercicios/ex066.py
@@ -0,0 +1,8 @@
+soma = c = 0
+while True:
+    num = int(input("Digite um valor (999 para parar): "))
+    if num == 999:
+        break
+    soma += num
+    c += 1
+print(f"A soma dos {c} valores foi {soma}")
