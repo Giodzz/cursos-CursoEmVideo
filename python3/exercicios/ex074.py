daff62320400557b1850bc24e143e897627426fa
Giodzz <giordanabbucci@gmail.com>
2023-08-22 13:14:09 -0300

    ex074: Maior e menor valores em tupla

---
+++ python3/exercicios/ex074.py
@@ -0,0 +1,6 @@
+from random import randint
+
+numeros = (randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100), randint(0, 100))
+print(f"Sorteei os valores: {numeros}")
+print(f"Maior: {max(numeros)}")
+print(f"Menor: {min(numeros)}")
