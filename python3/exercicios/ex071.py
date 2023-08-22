06c742207df58b45091df0376e5f163040dc5b13
Giodzz <giordanabbucci@gmail.com>
2023-08-22 10:13:37 -0300

    ex071: simulador de caixa eletrônico

---
+++ python3/exercicios/ex071.py
@@ -0,0 +1,24 @@
+print("="*40)
+print(f"{'BANCO CEV':^40}")
+print("="*40)
+
+sacar = int(input("Qual valor você quer sacar? "))
+total = sacar
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        print(f"Total de {totced} cédulas de R${ced:.2f}")
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+
+        if total == 0:
+            break
