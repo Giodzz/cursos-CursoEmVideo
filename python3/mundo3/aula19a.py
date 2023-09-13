ad28962d00599d41ef55083d374f230595107ed1
Giodzz <giordanabbucci@gmail.com>
2023-09-13 11:43:43 -0300

    aula19: dicionários

---
+++ python3/mundo3/aula19a.py
@@ -0,0 +1,7 @@
+pessoas = {"nome": "Gustavo", "idade": 22}
+pessoas['nome'] = 'Giordana'
+pessoas['nasc'] = 2004
+print(f"O {pessoas['nome']} tem {pessoas['idade']} anos")
+print(pessoas.values())
+print(pessoas.keys())
+print(pessoas.items())
